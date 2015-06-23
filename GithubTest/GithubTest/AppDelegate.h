7d9bf71ade3ab52987cb58054615c99040246a81
jay8018 <showtek@ShowTekdeMac-mini.local>
2015-06-23 11:49:24 +0800

    test1
    test1

---
+++ GithubTest/GithubTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GithubTest
+//
+//  Created by ShowTek on 15-6-23.
+//  Copyright (c) 2015年 net.showmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GithubTest/GithubTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GithubTest
+//
+//  Created by ShowTek on 15-6-23.
+//  Copyright (c) 2015年 net.showmap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
